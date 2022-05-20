965989c9d877fb79145c15e284b65222c5fade8d
DanilPidhainyi <61890854+DanilPidhainyi@users.noreply.github.com>
2022-05-20 11:39:54 +0300

    Add utils

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,47 @@
-import './App.css';
+import React, { useState, useEffect, useRef } from 'react';
+import './App.scss';
+import InputField from "../InputField";
+import Preview from "../Preview";
+import Menu from "../Menu";
+import { getHtml, saveToLocalStorage, createFile } from "../../utils";
 
-function App() {
+const App = () => {
+  const [text, setText] = useState('');
+  const appEl = useRef(null);
+
+  useEffect(() => {
+    const newText = localStorage.getItem('text');
+    setText(newText ? newText : '');
+  }, []);
+
+  const onChange = (event) => {
+    setText(event.currentTarget.value);
+  };
+
+  const onLeft = () => {
+    appEl.current.classList.add('tab--open');
+    appEl.current.classList.remove('tab--close');
+  };
+
+  const onRight = () => {
+    appEl.current.classList.remove('tab--open');
+    appEl.current.classList.add('tab--close');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app" className={'app'} ref={appEl}>
+      <InputField
+        value={ text }
+        onChange={ onChange } />
+      <Preview
+        htmlData={ () => getHtml(text) }
+        onLeft={ onLeft }
+        onRight={ onRight } />
+      <Menu
+        getFile={ () => createFile(text) }
+        saveToLocalStorage={ () => saveToLocalStorage(text) }/>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Menu.scss';
+import LinkIcon from './svg/link.svg'
+import SaveIcon from './svg/save.svg'
+import DownloadIcon from './svg/download.svg'
+
+const Menu = ({ saveToLocalStorage, getFile }) => (
+  <div className="menu">
+    <div title={'link'}>
+      <LinkIcon/>
+    </div>
+    <div title={'Save to local storage'} onClick={saveToLocalStorage}>
+      <SaveIcon/>
+    </div>
+    <div title={'Save to Downloads'}>
+      <a href={getFile()} download={'index'}>
+        <DownloadIcon/>
+      </a>
+    </div>
+  </div>
+);
+
+export default Menu;
